165c11ae8606bf0b2e8eee4a84ca455420ef0a2a
Putry Andini <putry19@mhs.akba.ac.id>
2021-10-31 19:06:06 +0800

    Commit Pertama

---
+++ tugas-02-01/dini.php
@@ -0,0 +1,9 @@
+<?php
+/**
+* Ini adalah nilai awal variabel
+*/
+$nama = "Putry Andini";
+$usia = 20;
+echo "<p>Hai, perkenalkan nama saya adalah ".$nama.", dan saya
+berusia ".$usia." tahun<p>";
+?>
